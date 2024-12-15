198d3d49ab86697d6dbdb8d9b020d46467325b18
Branyoe <branyoe.dev@gmail.com>
2024-12-14 22:45:23 -0600

    p1-s7-drf

--- app/movies/admin.py
+++ app/movies/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-# Register your models here.
+from .models import CustomUser, Movie
+
+
+@admin.register(CustomUser)
+class UserAdmin(DefaultUserAdmin):
+    pass
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    fields = ("title", "genre", "year", "created_date", "updated_date")
+    list_display = ("title", "genre", "year", "created_date", "updated_date")
+    search_fields = ("title", "genre", "year")
+    readonly_fields = ("created_date", "updated_date")
+    list_filter = ("genre", "year")
+    ordering = ("-created_date", "-updated_date", "title", "genre", "year")
+    
