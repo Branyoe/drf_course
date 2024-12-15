97f9b6b5be1022aba2adda5df14f1e82788243c8
Branyoe <branyoe.dev@gmail.com>
2024-12-14 23:35:14 -0600

    p1-s8-RestFul: viewset refactor

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import MovieList, MovieDetail
+from .views import MoviesViewSet
+
+
+router = DefaultRouter()
+router.register(r'movies', MoviesViewSet)
+
 
 urlpatterns = [
-    path("api/movies/", MovieList.as_view()),
-    path("api/movies/<int:pk>/", MovieDetail.as_view())
+    # path("api/movies/", MovieList.as_view()),
+    # path("api/movies/<int:pk>/", MovieDetail.as_view())
+    path('api/', include(router.urls))
 ]
